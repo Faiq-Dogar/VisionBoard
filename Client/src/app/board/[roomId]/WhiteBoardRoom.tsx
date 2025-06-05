dc303eb536b1d845b1b4c6a5f3c0d70616a08701
Faiq-Dogar <faiqd17@gmail.com>
2025-06-06 02:00:56 +0500

    Custom color changing of a line segment 6/June/2025

--- Client/src/app/board/[roomId]/WhiteBoardRoom.tsx
+++ Client/src/app/board/[roomId]/WhiteBoardRoom.tsx
@@ -1,19 +1,32 @@
 "use client";
 
-import { Stage, Layer, Line } from "react-konva";
+import { Stage, Layer, Line, Circle } from "react-konva";
 import { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
+import PaletteIcon from "@mui/icons-material/Palette";
+import { Divider } from "@mui/material";
+const lineColorOptions = [
+  { name: "black", value: "#000000" },
+  { name: "red", value: "#ee4444" },
+  { name: "orange", value: "#f1a00a" },
+  { name: "green", value: "#14b781" },
+  { name: "blue", value: "#3c81f8" },
+  { name: "indigo", value: "#6167ec" },
+  { name: "pink", value: "#ea489d" },
+];
 
 const WhiteBoardRoom = () => {
   const [lines, setLines] = useState<any[]>([]);
+  const [circlePos, setCirclePos] = useState({ x: 200, y: 200 });
   const isDrawing = useRef(false);
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [lineColor, setLineColor] = useState<string>("#000");
 
   useEffect(() => {
     const updateSize = () => {
       setDimensions({
         width: window.innerWidth,
-        height: window.innerHeight,
+        height: window.innerHeight - 150,
       });
     };
     updateSize();
@@ -47,7 +60,31 @@ const WhiteBoardRoom = () => {
 
   return (
     <>
-      <div className="relative h-screen w-screen bg-gradient-to-br from-[#f0f4ff] via-white to-[#f4f7ff] overflow-hidden flex flex-col items-center justify-center text-center px-4">
+      <div className="relative h-screen w-screen bg-gradient-to-br from-[#f0f4ff] via-white to-[#f4f7ff] overflow-hidden flex flex-col justify-center px-4">
+        <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          // className="text-7xl md:text-8xl font-black bg-gradient-to-r from-slate-900 via-indigo-900 to-purple-900 bg-clip-text text-transparent mb-4 leading-tight"
+          className="text-6xl font-extrabold text-gray-900 drop-shadow-md text-center"
+        >
+          VisionBoard
+        </motion.h1>
+        <Divider />
+        <div className="p-4">
+          <label className="mr-2 font-medium text-black">Choose Color:</label>
+          {lineColorOptions.map((color) => (
+            <input
+              key={color.name}
+              type="color"
+              value={color.value}
+              className="cursor-pointer w-7 h-5 border-none px-1"
+              onChange={(e) => setLineColor(e.target.value)}
+            />
+          ))}
+        </div>
+        <Divider />
+
         <Stage
           width={dimensions.width}
           height={dimensions.height}
@@ -60,13 +97,26 @@ const WhiteBoardRoom = () => {
               <Line
                 key={i}
                 points={line.points}
-                stroke="#000"
-                strokeWidth={2}
+                stroke={lineColor} //color
+                strokeWidth={2} //thickness
                 tension={0.5}
                 lineCap="round"
                 globalCompositeOperation="source-over"
               />
             ))}
+
+            {/* <Circle
+              x={circlePos.x}
+              y={circlePos.y}
+              radius={30}
+              fill="skyblue"
+              stroke="navy"
+              strokeWidth={2}
+              draggable
+              onDragEnd={(e) =>
+                setCirclePos({ x: e.target.x(), y: e.target.y() })
+              }
+            /> */}
           </Layer>
         </Stage>
       </div>
